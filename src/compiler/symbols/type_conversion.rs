c4e24eeef3c338db8ad77a92e3f966cd49d91d8d
hydroper <matheusdiasdesouzads@gmail.com>
2024-02-02 09:15:21 -0300

    Update

--- src/compiler/symbols/type_conversion.rs
+++ src/compiler/symbols/type_conversion.rs
@@ -2,4 +2,43 @@ use crate::ns::*;
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum TypeConversionRelationship {
+    // Implicit
+    FromAny,
+    ToAny,
+    N1ToN2,
+    FromNonNullableToCovariantType,
+    FromNonNullableToNullableCovariantType,
+    FromNullableToNullableCovariantType,
+    FromTToNullableT,
+    FromInterfaceToObject,
+    FromInterfaceToNullableObject,
+
+    // Explicit
+    ThroughToProxy,
+    FromNonNullableToContravariantType,
+    FromNullableToNonNullableContravariantType,
+    ArrayToContravariantArray,
+    ArrayToCovariantArray,
+    BetweenNumberTypes,
+    FromStringToEnum,
+    FromNumberToEnum,
+    FromCharToInt,
+    FromIntToChar,
+    FromTypeParameter,
+}
+
+pub struct TypeConversions<'a>(pub &'a mut SymbolHost);
+
+impl<'a> TypeConversions<'a> {
+    pub fn implicit_constant_conversion(&mut self, value: &Symbol, target_type: &Symbol) -> Option<Symbol> {
+        ()
+    }
+
+    pub fn implicit_conversion(&mut self, value: &Symbol, target_type: &Symbol, optional: bool) -> Option<Symbol> {
+        ()
+    }
+
+    pub fn explicit_conversion(&mut self, value: &Symbol, target_type: &Symbol, optional: bool) -> Option<Symbol> {
+        ()
+    }
 }
